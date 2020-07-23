2fd2657facce33080236fa49ffc750f7ae0cdf3c
William Finnigan <wjafinnigan@gmail.com>
2020-07-23 14:09:39 +0100

    versioning

--- retrobiocat_web/__init__.py
+++ retrobiocat_web/__init__.py
@@ -1,6 +1,7 @@
 import datetime
+from retrobiocat_web.version import version
 
-__version__ = '0.10.0'
+__version__ = version
 
 from retrobiocat_web.retro.generation.network_generation.network import Network
 
