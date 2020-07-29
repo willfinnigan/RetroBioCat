0b3aeddd09835db388674d2e4c1af9f3496513e9
William Finnigan <wjafinnigan@gmail.com>
2020-07-29 14:44:09 +0100

    Excel upload for sequence data contribution

--- retrobiocat_web/app/contributions/routes/ajax/sequences_ajax.py
+++ retrobiocat_web/app/contributions/routes/ajax/sequences_ajax.py
@@ -1,10 +1,15 @@
 from retrobiocat_web.app.contributions import bp
-from flask import request, jsonify
+from flask import request, jsonify, flash
 from flask_security import roles_required, current_user
-from retrobiocat_web.mongo.models.biocatdb_models import Sequence, Activity
+from retrobiocat_web.mongo.models.biocatdb_models import Sequence, Activity, EnzymeType, Paper
 from retrobiocat_web.app.app import user_datastore
 from distutils.util import strtobool
 from retrobiocat_web.app.contributions.functions import paper_status
+from werkzeug.utils import secure_filename
+import os
+import pandas as pd
+import numpy as np
+from distutils.util import strtobool
 
 def seqs_of_type(enzyme_type):
     sequences = Sequence.objects(enzyme_type=enzyme_type).distinct('enzyme_name')
@@ -32,6 +37,18 @@ def get_sequences_of_type():
     result = seqs_of_type(enzyme_type)
 
     return jsonify(result=result)
+
+def sequence_check(sequence):
+    amino_acids_list = ['X', 'V', 'G', 'F', 'E', 'N', 'P', 'Q', 'M', 'K', 'T', 'S', 'W', 'A', 'R', 'D', 'L', 'Y', 'H',
+                        'I', 'C', '*']
+
+    aa_check = True
+    for letter in sequence:
+        if letter.upper() not in amino_acids_list:
+            aa_check = False
+
+    return aa_check
+
 
 @bp.route('/_save_edited_sequence', methods=['GET', 'POST'])
 @roles_required('contributor')
@@ -82,17 +99,9 @@ def save_edited_sequence():
     elif self_assigned == False and seq.owner == user:
         seq.owner = None
 
-    amino_acids_list = ['X', 'V', 'G', 'F', 'E', 'N', 'P', 'Q', 'M', 'K', 'T', 'S', 'W', 'A', 'R', 'D', 'L', 'Y', 'H',
-                        'I', 'C', '*']
-
     sequence = sequence.replace('\n', '')
 
-    aa_check = True
-    for letter in sequence:
-        if letter.upper() not in amino_acids_list:
-            aa_check = False
-
-    if aa_check == False:
+    if sequence_check(sequence) == False:
         status = 'danger'
         msg = 'Could not update sequence'
         issues.append('Protein sequence contains non amino acid characters')
@@ -276,3 +285,143 @@ def update_seq_papers_status(enzyme_name):
 
         paper.status = status
         paper.save()
+
+
+@bp.route('/_upload_sequence_excel',methods=['GET', 'POST'])
+@roles_required('contributor')
+def upload_sequence_excel():
+    issues = []
+    if request.method != 'POST':
+        issues.append('Method is not POST')
+    else:
+        excel_file = request.files['file_seq']
+        paper = Paper.objects(id=request.form['paper_id_field'])[0]
+        filename = secure_filename(excel_file.filename)
+        if filename[-5:] == '.xlsx':
+            excel_file.save(filename)
+            df = pd.read_excel(filename)
+            data_list = process_uploaded_excel(df)
+            os.remove(filename)
+
+            save_issues = save_or_add_seqs(data_list, paper)
+
+            if len(save_issues) == 0:
+                result = {'status': 'success',
+                          'msg': 'Sequences saved and added to paper',
+                          'issues': []}
+                flash("Sequences saved and added to paper", "success")
+            else:
+                result = {'status': 'warning',
+                          'msg': 'Sequences saved with some issues:',
+                          'issues': save_issues}
+                flash("Sequences saved with some issues", "warning")
+            return jsonify(result=result)
+        else:
+            issues.append('File does not end in .xlsx')
+
+    result = {'status': 'danger',
+              'msg': 'Error processing file',
+              'issues': issues}
+    return jsonify(result=result)
+
+
+def process_uploaded_excel(df):
+    cols = ['enzyme_type', 'enzyme_name', 'other_names', 'sequence',
+            'sequence_unavailable', 'accession', 'structure',
+            'mutant_of', 'notes']
+
+    cols_to_keep = [c for c in cols if c in list(df.columns)]
+    df = df[cols_to_keep]
+    df.replace(np.nan, '', inplace=True)
+
+    data_list = df.to_dict(orient='records')
+
+    return data_list
+
+def save_or_add_seqs(data_list, paper):
+    user = user_datastore.get_user(current_user.id)
+    issues = []
+    enzyme_types = EnzymeType.objects().distinct('enzyme_type')
+
+    for seq_dict in data_list:
+        if 'sequence_unavailable' in seq_dict:
+            if seq_dict['sequence_unavailable'] == '':
+                seq_dict['sequence_unavailable'] = 'False'
+
+        if 'structure' in seq_dict:
+            if seq_dict['structure'] == '':
+                seq_dict['structure'] = 'False'
+
+        if 'sequence' in seq_dict:
+            seq_dict['sequence'] = seq_dict['sequence'].replace('\n', '')
+
+        if seq_dict.get('enzyme_name','') == '':
+            issues.append(f"Sequence must have a name")
+        else:
+            if len(Sequence.objects(enzyme_name=seq_dict['enzyme_name'])) == 0:
+                if seq_dict.get('enzyme_type', '') not in enzyme_types:
+                    print(f"Enzyme type {seq_dict.get('enzyme_type', '')} does not exist")
+                    issues.append(f"Enzyme type {seq_dict.get('enzyme_type', '')} does not exist")
+
+                elif sequence_check(seq_dict.get('sequence','')) == False:
+                    print(f"Amino acid sequence for {seq_dict['enzyme_name']} uses incorrect amino acid characters")
+                    issues.append(f"Amino acid sequence for {seq_dict['enzyme_name']} uses incorrect amino acid characters")
+
+                else:
+                    print('Creating new sequence..')
+                    seq = Sequence(enzyme_name=seq_dict['enzyme_name'],
+                                   enzyme_type=seq_dict['enzyme_type'],
+                                   other_names=seq_dict.get('other_names', '').split(', '),
+                                   sequence=seq_dict.get('sequence', ''),
+                                   sequence_unavailable=strtobool(seq_dict.get('sequence_unavailable','False')),
+                                   accession=seq_dict.get('accession', ''),
+                                   structure=strtobool(seq_dict.get('structure','False')),
+                                   mutant_of=seq_dict.get('mutant_of', ''),
+                                   notes=seq_dict.get('notes', ''),
+                                   papers=[paper],
+                                   owner=user)
+                    seq.save()
+
+                    print(seq)
+
+            else:
+                seq = Sequence.objects(enzyme_name=seq_dict['enzyme_name'])[0]
+                if paper not in seq.papers:
+                    seq.papers.append(paper)
+
+                if seq.owner == user or seq.owner is None:
+                    seq.owner = user
+                    other_names = seq_dict.get('other_names', '').split(', ')
+                    seq.other_names.extend(other_names)
+
+                    if (seq.sequence is None or seq.sequence == ''):
+                        seq.sequence = seq_dict.get('sequence', '')
+
+                    if strtobool(seq_dict.get('sequence_unavailable','False')) == True:
+                        seq.sequence_unavailable = True
+
+                    if (seq.accession is None or seq.accession == ''):
+                        seq.accession = seq_dict.get('accession', '')
+
+                    if strtobool(seq_dict.get('structure','False')) == True:
+                        seq.structure = True
+
+                    if (seq.mutant_of is None or seq.mutant_of == ''):
+                        seq.mutant_of = seq_dict.get('mutant_of', '')
+
+                    if (seq.notes is None or seq.notes == ''):
+                        seq.notes = seq_dict.get('notes', '')
+
+                else:
+                    print('Sequence already exists but owned by another user - added to paper, but no data updated')
+                    issues.append('Sequence already exists but owned by another user - added to paper, but no data updated')
+
+                seq.save()
+
+    return issues
+
+
+
+
+
+
