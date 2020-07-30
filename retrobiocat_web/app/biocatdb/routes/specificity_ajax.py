9a3625656c2a0a11c017ef67a841743d080669cf
William Finnigan <wjafinnigan@gmail.com>
2020-07-30 22:22:59 +0100

    changed to activity modal

--- retrobiocat_web/app/biocatdb/routes/specificity_ajax.py
+++ retrobiocat_web/app/biocatdb/routes/specificity_ajax.py
@@ -29,13 +29,42 @@ def load_single_activity_data():
     else:
         added_by = ''
 
+    if activity.substrate_1_conc is None:
+        sub1_conc = ''
+    else:
+        sub1_conc = activity.substrate_1_conc
+        if sub1_conc.replace('.', '').isnumeric():
+            sub1_conc += ' mM'
+
+    if activity.substrate_2_conc is None:
+        sub2_conc = ''
+    else:
+        sub2_conc = activity.substrate_2_conc
+        if sub2_conc.replace('.', '').isnumeric():
+            sub2_conc += ' mM'
+
+    if activity.biocat_conc is None:
+        biocat_conc = ''
+    else:
+        biocat_conc = activity.biocat_conc
+        if biocat_conc.replace('.', '').isnumeric():
+            biocat_conc += ' mg/ml'
+
     result = {'enzyme_name': activity.enzyme_name,
               'enzyme_type': activity.enzyme_type,
               'reaction': activity.reaction,
               'sub1_img': sub1,
               'sub2_img': sub2,
               'prod_img': prod,
-              'added_by': added_by}
+              'added_by': added_by,
+              'sub1_conc': sub1_conc,
+              'sub2_conc': sub2_conc,
+              'biocat_conc': biocat_conc,
+              'formulation': activity.formulation}
+
+    for key in result:
+        if result[key] is None:
+            result[key] = ''
 
     print(result)
 
