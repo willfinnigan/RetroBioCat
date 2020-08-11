08d843914d320eb45dd8f0afd717f6cf92f849cf
William Finnigan <wjafinnigan@gmail.com>
2020-08-11 20:27:14 +0100

    contributions page and column widths

---
+++ retrobiocat_web/app/biocatdb/routes/query_db/leaderboards.py
@@ -0,0 +1,39 @@
+from flask import render_template, jsonify, session, request, redirect, url_for
+from retrobiocat_web.app.biocatdb import bp
+import mongoengine as db
+from retrobiocat_web.app.biocatdb.functions import sequence_table
+from retrobiocat_web.mongo.models.biocatdb_models import EnzymeType, Paper, Sequence, Activity
+from retrobiocat_web.mongo.models.user_models import User, Role
+from retrobiocat_web.app.biocatdb.forms import SequenceSearch
+import itertools
+
+
+@bp.route("/leaderboard", methods=["GET"])
+def leaderboard():
+    contributor_role = Role.objects(name='contributor')[0]
+    contributors = User.objects(roles=contributor_role)
+
+    papers_dict = {}
+    sequence_dict = {}
+    activity_dict = {}
+    for user in contributors:
+        username = f"{user.first_name} {user.last_name}, {user.affiliation}"
+        num_papers = len(Paper.objects(owner=user))
+        num_sequences = len(Sequence.objects(owner=user))
+        num_activity = len(Activity.objects(added_by=user))
+        papers_dict[username] = num_papers
+        sequence_dict[username] = num_sequences
+        activity_dict[username] = num_activity
+
+    papers_dict = {k: v for k, v in sorted(papers_dict.items(), key=lambda item: item[1], reverse=True)}
+    papers_dict = {k: v for k, v in papers_dict.items() if v != 0}
+    sequence_dict = {k: v for k, v in sorted(sequence_dict.items(), key=lambda item: item[1], reverse=True)}
+    sequence_dict = {k: v for k, v in sequence_dict.items() if v != 0}
+    activity_dict = {k: v for k, v in sorted(activity_dict.items(), key=lambda item: item[1], reverse=True)}
+    activity_dict = {k: v for k, v in activity_dict.items() if v != 0}
+
+
+    return render_template('leaderboard.html',
+                           top_papers=papers_dict,
+                           top_sequences=sequence_dict,
+                           top_activity=activity_dict)
