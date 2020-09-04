90fb13dfec5de1f5c4fd03e2ae67eb96a8c23102
William Finnigan <wjafinnigan@gmail.com>
2020-09-04 15:50:42 +0100

    show number of reactions

--- retrobiocat_web/app/retrobiocat/routes/reactions/reaction_routes.py
+++ retrobiocat_web/app/retrobiocat/routes/reactions/reaction_routes.py
@@ -4,6 +4,7 @@ from retrobiocat_web.app.retrobiocat import bp
 from retrobiocat_web.app.retrobiocat.functions.get_images import rxntosvg
 from retrobiocat_web.retro.generation.load.rxn_class import RetroBioCat_Reactions
 
+
 @bp.route('/reaction_rules')
 def reaction_rules():
     rxns = RetroBioCat_Reactions()
@@ -12,13 +13,20 @@ def reaction_rules():
     images = {}
     strings = {}
     rules_by_type = rxns.rules_by_type
+    num_reactions = 0
+    num_smarts = 0
     for rule_type in rules_by_type:
         reaction_names = rxns.rules_by_type[rule_type]
         for name in reaction_names:
+            num_reactions += 1
             list_rxns = rxns.rxns[name]
+            num_smarts += len(list_rxns)
             images[name] = rxntosvg(list_rxns,rxnSize=(450,150))
             strings[name] = rxns.rxns_strings[name]
+
     return render_template('reactions/reaction_rules.html',
                             rule_types=rules_by_type,
                             rule_images=images,
-                            reaction_enzymes=rxns.reaction_enzyme_map)
+                            reaction_enzymes=rxns.reaction_enzyme_map,
+                            num_reactions=num_reactions,
+                            num_smarts=num_smarts)
