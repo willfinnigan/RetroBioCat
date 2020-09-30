9ec72bd41b95774305758ef43e82b035d325c6b6
William Finnigan <wjafinnigan@gmail.com>
2020-09-30 11:42:48 +0100

    ssn clustering

--- retrobiocat_web/app/biocatdb/routes/analysis/ssn.py
+++ retrobiocat_web/app/biocatdb/routes/analysis/ssn.py
@@ -16,13 +16,12 @@ def ssn(enzyme_type):
                                          'iterations': 100},
                        "barnesHut": {"gravitationalConstant": -16000,
                                      "springLength": 300,
-                                     "springConstant": 0.1,
-                                     "damping": 0.1,
+                                     "springConstant": 0.015,
+                                     "damping": 0.2,
                                      "avoidOverlap": 0.1},
                        "maxVelocity": 49,
                        "minVelocity": 0.75,
                        "solver": "barnesHut"}
-
 
     interaction_options = {'tooltipDelay': 0,
                            'hideEdgesOnDrag': True}
