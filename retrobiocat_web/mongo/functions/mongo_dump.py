24bfa491023b7aa0690d1ce04dd8da52adb5bb10
William Finnigan <wjafinnigan@gmail.com>
2020-12-07 15:13:23 +0000

    refactor to db_analysis, auto jobs

--- retrobiocat_web/mongo/functions/mongo_dump.py
+++ retrobiocat_web/mongo/functions/mongo_dump.py
@@ -5,6 +5,6 @@ from flask import current_app
 def execute_mongo_dump():
     print("Executing mongo dump..")
     output_path = str(Path(__file__).parents[1]) + '/mongo_dump'
-    command = f"mongodump --host='{current_app.config['MONGODB_HOST']}:{current_app.config['MONGODB_PORT']}' -o {output_path} --db={current_app.config['MONGODB_DB']} --gzip &"
+    command = f"mongodump --host='{current_app.config['MONGODB_HOST']}:{current_app.config['MONGODB_PORT']}' --out {output_path} --db={current_app.config['MONGODB_DB']} --gzip &"
     print(f"CMD = {command}")
     sp.run(command, shell=True)
