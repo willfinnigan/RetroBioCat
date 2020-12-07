6fcaf9d6a9e5f4ce42561227c83296e99da90b69
William Finnigan <wjafinnigan@gmail.com>
2020-12-07 21:20:14 +0000

    mongo_dump

--- retrobiocat_web/mongo/functions/mongo_dump.py
+++ retrobiocat_web/mongo/functions/mongo_dump.py
@@ -7,9 +7,9 @@ def execute_mongo_dump():
     print("Executing mongo dump..")
     output_path = str(Path(__file__).parents[1]) + '/mongo_dump/mongo_dump.gz'
     if current_app.config['MONGODB_HOST'] == 'localhost':
-        command = f'''mongodump --uri="mongodb://localhost:{current_app.config['MONGODB_PORT']}" --db={current_app.config['MONGODB_DB']} --gzip --archive={output_path} --verbose'''
+        command = f'''mongodump --uri="mongodb://localhost:{current_app.config['MONGODB_PORT']}" --gzip --archive={output_path} --verbose'''
     else:
-        command = f'''mongodump --uri="{current_app.config['MONGODB_HOST']}:{current_app.config['MONGODB_PORT']}" --db={current_app.config['MONGODB_DB']} --gzip --archive={output_path} --verbose'''
+        command = f'''mongodump --uri="{current_app.config['MONGODB_HOST']}:{current_app.config['MONGODB_PORT']}" --gzip --archive={output_path} --verbose'''
     print(f"CMD = {command}")
     sp.run(command, shell=True)
 
