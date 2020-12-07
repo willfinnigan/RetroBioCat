77a9ca7ad336f6e76db8cd82ba76dff8543083b8
William Finnigan <wjafinnigan@gmail.com>
2020-12-07 17:30:31 +0000

    mongo dump

--- retrobiocat_web/mongo/functions/mongo_dump.py
+++ retrobiocat_web/mongo/functions/mongo_dump.py
@@ -5,7 +5,7 @@ from flask import current_app
 def execute_mongo_dump():
     print("Executing mongo dump..")
     output_path = str(Path(__file__).parents[1]) + '/mongo_dump/mongo_dump.gz'
-    command = f'''mongodump --uri="{current_app.config['MONGODB_HOST']}:{current_app.config['MONGODB_PORT']}" --db={current_app.config['MONGODB_DB']} --gzip --archive={output_path} &'''
+    command = f'''mongodump --uri="{current_app.config['MONGODB_HOST']}:{current_app.config['MONGODB_PORT']}" --gzip --archive={output_path} --verbose'''
     print(f"CMD = {command}")
     sp.run(command, shell=True)
 
